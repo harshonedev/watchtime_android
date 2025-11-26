c101a4d6a16e5d6217f2f6110303efb8466a1b6e
Harsh <harshrajput2906@gmail.com>
2025-11-27 00:46:42 +0530

    Add SupabaseAuthService for Google authentication and session management; introduce TMDB constants in network and utils modules

---
+++ auth/data/src/main/java/com/app/auth/data/services/SupabaseAuthService.kt
@@ -0,0 +1,49 @@
+package com.app.auth.data.services
+
+import android.util.Log
+import com.app.core.utils.failures.Failure
+import io.github.jan.supabase.SupabaseClient
+import io.github.jan.supabase.auth.auth
+import io.github.jan.supabase.auth.providers.Google
+import io.github.jan.supabase.auth.providers.builtin.IDToken
+import io.github.jan.supabase.auth.status.SessionStatus
+import io.github.jan.supabase.auth.user.UserInfo
+import io.github.jan.supabase.exceptions.RestException
+
+class SupabaseAuthService (
+    private val supabase : SupabaseClient
+) {
+
+    companion object {
+        const val TAG = "SupabaseAuthService.kt"
+    }
+
+    suspend fun authWithGoogle(googleIdToken : String, rawNonce : String) {
+        try {
+
+            supabase.auth.signInWith(IDToken){
+                idToken = googleIdToken
+                provider = Google
+                nonce = rawNonce
+            }
+        } catch (e: RestException) {
+            Log.e(TAG, "Error on supabase auth: ${e.localizedMessage}")
+            throw Failure.AuthenticationError(e.message)
+        } catch (e : Exception) {
+            Log.e(TAG, "Error on supabase auth with google sign in: ${e.localizedMessage}")
+            throw Failure.AuthenticationError(e.message)
+        }
+    }
+
+     fun getSupabaseUser() : UserInfo? {
+         return supabase.auth.currentUserOrNull()
+     }
+
+    suspend fun signOut() {
+        supabase.auth.signOut()
+    }
+
+    fun isLoggedIn() : Boolean {
+        return supabase.auth.currentUserOrNull() != null
+    }
+}
---
+++ core/network/src/main/java/com/app/core/network/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.app.core.network.util
+
+object Constants {
+    const val TMDB_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/"
+    const val TMDB_BASE_URL = "https://api.themoviedb.org/3/"
+    const val TMDB_API_KEY =
+        "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYTMwMGQ1OTBlMWYxNWMxMThkOWJlYmQ1ZjBiNWJiNyIsIm5iZiI6MTczNzAxMDg3OC41NzMsInN1YiI6IjY3ODhhZWJlMTQwMzcwMWM5OTFkMzc3ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.764Xm3FBDDU7p93dcigdKnm06z1JTK76BdSkaUyDp_g" // Replace with your actual API key
+}
